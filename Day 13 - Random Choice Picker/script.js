ff077879f9b57ef3c485dac74f44cd5aeffd08a8
gloriayordanova <raevagloria@gmail.com>
2025-02-22 23:52:06 +0000

    Final Version

---
+++ Day 13 - Random Choice Picker/script.js
@@ -0,0 +1,8 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+})
