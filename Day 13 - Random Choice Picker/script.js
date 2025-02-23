c948e72ec19885316e7ec6401abf563a67386712
gloriayordanova <raevagloria@gmail.com>
2025-02-23 17:30:13 +0000

    Final Version

--- Day 13 - Random Choice Picker/script.js
+++ Day 13 - Random Choice Picker/script.js
@@ -6,3 +6,13 @@ textarea.focus()
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
 })
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+    })
+}
